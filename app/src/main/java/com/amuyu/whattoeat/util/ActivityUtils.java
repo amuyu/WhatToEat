04162bef25c134ba90734adb8e3e4393a10bcdb9
amuyu <hwajoong.song@gmail.com>
2017-11-02 17:48:09 +0900

    Add replacefragment function

--- app/src/main/java/com/amuyu/whattoeat/util/ActivityUtils.java
+++ app/src/main/java/com/amuyu/whattoeat/util/ActivityUtils.java
@@ -1,5 +1,7 @@
 package com.amuyu.whattoeat.util;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -23,5 +25,57 @@ public class ActivityUtils {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.add(frameId, fragment);
         transaction.commit();
+    }
+
+    public static <T extends Fragment> T replaceFragment(Context context, FragmentManager fragmentManager,
+                                                         FragmentOperation<T> operation, int frameId) {
+        Fragment fragment = Fragment.instantiate(context, operation.fragmentName, operation.arguments);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        if (operation.animationSet != null) {
+            if (operation.animationSet.length == 2) {
+                transaction.setCustomAnimations(
+                        operation.animationSet[0], operation.animationSet[1]);
+            } else if (operation.animationSet.length == 4) {
+                transaction.setCustomAnimations(
+                        operation.animationSet[0], operation.animationSet[1],
+                        operation.animationSet[2], operation.animationSet[3]);
+            }
+
+        }
+        transaction.replace(frameId, fragment);
+        if (operation.addToBackStack) transaction.addToBackStack(operation.fragmentName);
+        transaction.commit();
+        return (T) fragment;
+    }
+
+    public static class FragmentOperation<T extends Fragment> {
+        String fragmentName;
+        Bundle arguments;
+        boolean addToBackStack;
+        int[] animationSet;
+
+        private FragmentOperation() {
+        }
+
+        public static <T extends Fragment> FragmentOperation<T> create(Class<T> fragment) {
+            FragmentOperation fragmentOperation = new FragmentOperation();
+            fragmentOperation.fragmentName = fragment.getName();
+            return fragmentOperation;
+        }
+
+        public FragmentOperation<T> arguments(Bundle arguments) {
+            this.arguments = arguments;
+            return this;
+        }
+
+        public FragmentOperation<T> addToBackStack(boolean addToBackStack) {
+            this.addToBackStack = addToBackStack;
+            return this;
+        }
+
+        public FragmentOperation<T> animationSet(int... animationSet) {
+            this.animationSet = animationSet;
+            return this;
+        }
     }
 }
