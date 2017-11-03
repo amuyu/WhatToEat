1ace0a0226caa3bb69d5775bd73fec98c950ff9f
amuyu <hwajoong.song@gmail.com>
2017-11-03 15:48:23 +0900

    카카오톡링크 사용 추가

--- app/src/main/java/com/amuyu/whattoeat/view/SampleActivity.java
+++ app/src/main/java/com/amuyu/whattoeat/view/SampleActivity.java
@@ -12,11 +12,23 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.amuyu.logger.Logger;
 import com.amuyu.whattoeat.R;
+import com.kakao.kakaolink.v2.KakaoLinkResponse;
+import com.kakao.kakaolink.v2.KakaoLinkService;
+import com.kakao.message.template.ButtonObject;
+import com.kakao.message.template.ContentObject;
+import com.kakao.message.template.FeedTemplate;
+import com.kakao.message.template.LinkObject;
+import com.kakao.network.ErrorResult;
+import com.kakao.network.callback.ResponseCallback;
+import com.kakao.network.storage.ImageUploadResponse;
+import com.kakao.util.helper.MediaUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -43,7 +55,9 @@ public class SampleActivity extends AppCompatActivity {
         ((Button)findViewById(R.id.btnKakao)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                shareKakao();
+//                shareKakao();
+//                sendDefaultFeedTemplate();
+                uploadImage();
             }
         });
     }
@@ -152,5 +166,71 @@ public class SampleActivity extends AppCompatActivity {
 
             out.close();
         }
+    }
+
+    private void uploadImage() {
+
+
+        String mediaPath = Environment.getExternalStorageDirectory() + "/profile.jpg";
+
+        // Create the URI from the media
+        Logger.d("path!!:"+mediaPath);
+        File media = new File(mediaPath);
+        Uri uri = Uri.fromFile(media);
+
+        try {
+            File imageFile = new File(MediaUtils.getImageFilePathFromUri(uri, this));
+
+            KakaoLinkService.getInstance().uploadImage(this, false, imageFile, new ResponseCallback<ImageUploadResponse>() {
+                @Override
+                public void onFailure(ErrorResult errorResult) {
+                    com.kakao.util.helper.log.Logger.e(errorResult.toString());
+                    Toast.makeText(getApplicationContext(), errorResult.toString(), Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onSuccess(ImageUploadResponse result) {
+                    String imageUrl = result.getOriginal().getUrl();
+                    Logger.d("imageUrl:"+ imageUrl);
+                    sendDefaultFeedTemplate(imageUrl);
+                    Toast.makeText(SampleActivity.this, "Successfully uploaded image at " + imageUrl, Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (FileNotFoundException e) {
+            com.kakao.util.helper.log.Logger.e(e.toString());
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void sendDefaultFeedTemplate(String url) {
+        Logger.d("");
+        FeedTemplate params = FeedTemplate
+                .newBuilder(ContentObject.newBuilder("딸기 치즈 케익",
+                        url,
+                        LinkObject.newBuilder().setWebUrl("레스토랑에서 뭐 먹을지 알려줘")
+                                .setMobileWebUrl("https://developers.kakao.com").build())
+                        .setDescrption("#케익 #딸기 #삼평동 #카페 #분위기 #소개팅")
+                        .build())
+                .addButton(new ButtonObject("이 앱이 궁금해?", LinkObject.newBuilder()
+                        .setWebUrl("https://developers.kakao.com")
+                        .setMobileWebUrl("https://developers.kakao.com")
+                        .setAndroidExecutionParams("key1=value1")
+                        .setIosExecutionParams("key1=value1")
+                        .build()))
+                .build();
+
+
+        KakaoLinkService.getInstance().sendDefault(this, params, new ResponseCallback<KakaoLinkResponse>() {
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                Logger.d("");
+                com.kakao.util.helper.log.Logger.e(errorResult.toString());
+            }
+
+            @Override
+            public void onSuccess(KakaoLinkResponse result) {
+                Logger.d("");
+            }
+        });
     }
 }
