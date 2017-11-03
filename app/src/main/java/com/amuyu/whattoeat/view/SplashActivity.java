303c42ce3418053b30d3eef37fa66771c8334d40
amuyu <hwajoong.song@gmail.com>
2017-11-03 16:18:52 +0900

    Splash 화면 추가

---
+++ app/src/main/java/com/amuyu/whattoeat/view/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.amuyu.whattoeat.view;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.amuyu.whattoeat.MainActivity;
+import com.amuyu.whattoeat.R;
+import com.tsengvn.typekit.TypekitContextWrapper;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Observable.timer(2, TimeUnit.SECONDS)
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(TypekitContextWrapper.wrap(newBase));
+    }
+}
